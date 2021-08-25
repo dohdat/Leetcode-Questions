be66417cd5bce8d60682b79897a4724cf5a6223a
dohdat <dohdat@gmail.com>
2021-08-25 09:10:06 -0700

    Added hashmap to store res and its distance from target val

--- 16.3-sum-closest.js
+++ 16.3-sum-closest.js
@@ -10,8 +10,30 @@
  * @param {number} target
  * @return {number}
  */
-var threeSumClosest = function(nums, target) {
-    
+//calculates all sums, then find the closest one to target
+var threeSumClosest = function (nums, target) {
+    nums.sort((a, b) => a - b);
+    let res = [];
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+        let l = i + 1;
+        let r = nums.length - 1;
+        while (l < r) {
+            let sum = nums[i] + nums[l] + nums[r];
+            res.push(sum);
+            l++;
+            r--;
+        }
+    }
+    let min = Number.MAX_VALUE;
+    let hashMap = {};
+
+    res.forEach((item) => (min = Math.min(min, target - item)));
 };
 // @lc code=end
 
+// @after-stub-for-debug-begin
+module.exports = threeSumClosest;
+// @after-stub-for-debug-end
