c1ac3ddc5fabb5a17871a13d7495037081af05eb
dohdat <dohdat@gmail.com>
2021-09-10 13:38:07 -0700

    Added reduce method to calculate product of the array

--- 238.product-of-array-except-self.js
+++ 238.product-of-array-except-self.js
@@ -11,23 +11,14 @@
  */
 var productExceptSelf = function (nums) {
     let result = [];
-    for (let i = 0; i < nums.length; i++) {
-        const cloneArray = nums.slice(); // clone the array so we don't modify the original array
-        cloneArray.splice(i, 1); // remove the ith element from the array
-        result.push(multiply(cloneArray));
-    }
+    result = nums.map((cur, i, arr) => {
+        let temp = arr.slice(); //clone the original array
+        temp.splice(i, 1); // remove the ith element from the array
+        return temp.reduce((prod = 1, num) => prod * num);
+    });
     return result;
 };
-//helper function to multiply all elements in an array
-const multiply = (array) => {
-    var sum = 1;
-    for (let i = 0; i < array.length; i++) {
-        sum *= array[i];
-    }
-    return sum;
-};
 
-productExceptSelf([-1, 1, 0, -3, 3]);
 // @lc code=end
 
 // @after-stub-for-debug-begin
