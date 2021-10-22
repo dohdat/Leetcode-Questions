e247d0b7585187402acab0b5c537a2917a4929d7
dohdat <dohdat@gmail.com>
2021-10-22 10:28:48 -0700

    Added common patterns and methods in Javascript

---
+++ Common patterns.js
@@ -0,0 +1,30 @@
+//Create hashmap
+let hashMap = {};
+let arr = s.split(""); //Convert string to array
+arr.forEach((c) => (hashMap[c] = (hashMap[c] || 0) + 1));
+
+//Sort hashmap
+var keys = Object.keys(hashMap);
+keys.sort((a, b) => {
+    return hashMap[b] - hashMap[a];
+});
+
+//convert array to string
+let resStr = result.join("");
+
+// Check if an array includes a certain value among its enties
+str.includes(val);
+// Returns the position of the value, or -1 if not found.
+str.indexOf(val);
+// Removes whitespace from beginning and end. Useful for input fields or data cleaning.
+str.trim();
+// returns the part of the string between the start and end indexes:
+str.substring(startIndex, endIndex);
+// Adds to end of an array.
+arr.push(val);
+// Removes last value of an array and returns it.
+arr.pop();
+// Copies an array.
+arr.slice();
+// Makes a copy of an array at some start index (inclusive) and end index (exclusive).
+arr.slice(startIndex, endIndex);
