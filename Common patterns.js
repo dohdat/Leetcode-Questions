7ab8cff95467c6461df4bb439edc7710bc2a9321
dohdat <dohdat@gmail.com>
2021-10-22 11:21:05 -0700

    Added backtracking

--- Common patterns.js
+++ Common patterns.js
@@ -116,6 +116,27 @@ function findPairs(array, k) {
 // If asked for all permutations/subsets then
 //     - Backtracking
 
+//Example: find all possible permutations
+// Input: nums = [1,2,3]
+// Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+var permute = function(nums) {
+    let res = [];
+    dfs(nums, new Set(), res);
+    return res;
+}
+function dfs(nums, curr, res) {
+    if (curr.size == nums.length) {
+        res.push(Array.from(curr));
+        return;
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (curr.has(nums[i])) continue;
+        curr.add(nums[i]);
+        dfs(nums, curr, res);
+        curr.delete(nums[i]);
+    }
+}
+
 // If given a tree then
 //     - DFS
 //     - BFS
