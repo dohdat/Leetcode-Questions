b06d835df96f02a8dad74af86d60efa7e8427301
dohdat <dohdat@gmail.com>
2021-10-26 11:47:50 -0700

    Added invert binary tree using DFS and BFS

--- Common patterns.js
+++ Common patterns.js
@@ -277,3 +277,34 @@ var topKFrequent = function (nums, k) {
 
     return res;
 };
+
+//Invert Binary Tree
+// DFS
+function invertTree(root) {
+    const stack = [root];
+
+    while (stack.length) {
+        const n = stack.pop();
+        if (n != null) {
+            [n.left, n.right] = [n.right, n.left];
+            stack.push(n.left, n.right);
+        }
+    }
+
+    return root;
+}
+
+// BFS
+function invertTree(root) {
+    const queue = [root];
+
+    while (queue.length) {
+        const n = queue.shift();
+        if (n != null) {
+            [n.left, n.right] = [n.right, n.left];
+            queue.push(n.left, n.right);
+        }
+    }
+
+    return root;
+}
