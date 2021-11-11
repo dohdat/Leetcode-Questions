f881528d1c4e278001790e32307376f993844908
dohdat <dohdat@gmail.com>
2021-11-11 14:44:03 -0800

    Added subsets question

--- 78.subsets.js
+++ 78.subsets.js
@@ -10,20 +10,6 @@
  * @return {number[][]}
  */
 var subsets = function (nums) {
-<<<<<<< HEAD
-    let res = [[]];
-    for (let i = 1; i <= nums.length; i++) {
-        dfs(nums.slice(0, i), res, 0);
-    }
-};
-
-const dfs = (nums, res, len) => {
-    res.push(nums[len]);
-    len++;
-    dfs(nums, res, len);
-};
-
-=======
     let res = [];
     generateSubsets([], res, 0, nums);
     return res;
@@ -34,7 +20,6 @@ const generateSubsets = (path, res, i, nums) => {
         generateSubsets([...path, nums[index]], res, index + 1, nums);
     }
 };
->>>>>>> 4232567a018c12a84f25eaafc04ceceff530e3bf
 // @lc code=end
 
 // @after-stub-for-debug-begin
