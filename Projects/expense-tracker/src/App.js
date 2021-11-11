768471e2139ec7754bc66c3f6510765b14b34143
dohdat <dohdat@gmail.com>
2021-11-11 15:55:23 -0800

    Added Income and Expense Section

--- Projects/expense-tracker/src/App.js
+++ Projects/expense-tracker/src/App.js
@@ -5,7 +5,20 @@ function App() {
         <div className="main-container">
             <div className="main-header">Expense Tracker</div>
             <div className="main-content">
-                <div className="main-title">YOUR BALANCE</div>
+                <div>
+                    <div className="main-title">YOUR BALANCE</div>
+                    <span>$260</span>
+                </div>
+                <div>
+                    <div>
+                        <div className="main-title">INCOME</div>
+                        <span>$500</span>
+                    </div>
+                    <div>
+                        <div className="main-title">EXPENSE</div>
+                        <span>$240</span>
+                    </div>
+                </div>
             </div>
         </div>
     );
