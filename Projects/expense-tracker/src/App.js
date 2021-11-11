f3bcc427d5ae58a91ec563d91768fb767584583d
dohdat <dohdat@gmail.com>
2021-11-11 15:48:54 -0800

    Added title and header

--- Projects/expense-tracker/src/App.js
+++ Projects/expense-tracker/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-    return <div className="App">Hello World</div>;
+    return (
+        <div className="main-container">
+            <div className="main-header">Expense Tracker</div>
+            <div className="main-content">
+                <div className="main-title">YOUR BALANCE</div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
