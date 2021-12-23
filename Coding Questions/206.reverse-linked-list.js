15ee2f0814be1dc01a6ff6a28669bc793b3257f9
dohdat <dohdat@gmail.com>
2021-12-22 16:16:19 -0800

    Updated eslint rules

---
+++ Coding Questions/206.reverse-linked-list.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=206 lang=javascript
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    let prev = null;
+    let curr = head;
+    let next = null;
+    while (curr !== null) {
+        next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    head = prev;
+    return head;
+};
+// @lc code=end
+
+// @after-stub-for-debug-begin
+module.exports = reverseList;
+// @after-stub-for-debug-end
